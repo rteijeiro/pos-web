e81c39eba8778c6e4fb0b5dca4742f9bf7ba2912
Monica <mmarort302@g.educaand.es>
2025-04-07 08:36:10 +0200

    ultimos retoques a la funcion guardar pago

--- savePayment.php
+++ savePayment.php
@@ -1,22 +1,23 @@
 <?php
 include 'connection/db.php';
 include 'connection/controller.php';
-
+//response header
 header("Content-Type: application/json");
-
+//define the response in json
 $data = json_decode(file_get_contents("php://input"), true);
-
+//checks whether the data have been received
 if (!$data || !isset($data['date'], $data['total'])) {
     echo json_encode(["error" => "Faltan datos en la solicitud"]);
     exit;
 }
 
-// Convertir valores
+//convert values
 $date = $data['date'];
 $total = floatval($data['total']);
-
+//saves the payment in the database
 $result = savePayments($pdo, $date, $total);
 
+//if it returns true, the payment was saved
 if ($result) {
     echo json_encode(["message" => "Pago guardado con éxito"]);
 } else {
